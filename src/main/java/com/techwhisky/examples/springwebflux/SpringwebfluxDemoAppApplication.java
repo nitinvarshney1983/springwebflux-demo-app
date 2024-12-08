7b0cda4de2a691618f8bb79f6ded8b4719b3ee9c
nitinvarshney1983 <nitinvarshneykiet@gmail.com>
2024-12-08 15:49:52 +0530

    added all files

---
+++ src/main/java/com/techwhisky/examples/springwebflux/SpringwebfluxDemoAppApplication.java
@@ -0,0 +1,13 @@
+package com.techwhisky.examples.springwebflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringwebfluxDemoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringwebfluxDemoAppApplication.class, args);
+	}
+
+}
